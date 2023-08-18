119c565830a6cee3f9fe588a25363272063daa22
MSSerg <magasishe@gmail.com>
2023-08-18 14:52:36 +0300

    added readme and updated validation

--- app/controllers/wiki_controller.rb
+++ app/controllers/wiki_controller.rb
@@ -2,14 +2,12 @@
 
 class WikiController < ApplicationController
   def index
-    if params[:query]
-      if wiki_url_validation(params[:query])
-        data       = Wiki::Scrap.perform(params[:query])
-        @title     = data[:title]
-        @languages = data[:languages]
+    if params[:query] && wiki_url_validation(params[:query])
+      data       = Wiki::Scrap.perform(params[:query])
+      @title     = data[:title]
+      @languages = data[:languages]
 
-        render partial: 'wiki/result', locals: { title: @title, languages: @languages }
-      end
+      render partial: 'wiki/result', locals: { title: @title, languages: @languages }
     end
   end
 
@@ -18,6 +16,6 @@ class WikiController < ApplicationController
   def wiki_url_validation(url)
     parsed_url = URI.parse(url)
 
-    parsed_url.host.include?('wikipedia')
+    parsed_url.class.to_s.eql?('URI::HTTPS') && parsed_url.host.include?('wikipedia')
   end
 end
